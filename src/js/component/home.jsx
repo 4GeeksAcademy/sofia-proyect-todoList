b9330a88cb5fa3903dae4e48375117c4bba95210
Sofia Araujo Areal <163517593+Sofia28062001@users.noreply.github.com>
2024-07-10 16:21:07 +0000

    First Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,52 +1,113 @@
-import React, {useState} from "react";
-import 'bootstrap/dist/css/bootstrap.css';
+import React, {useState, useEffect} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-//create your first component
 const Home = () => {
+    
+    const [todoList, setTodoList] = useState([]);
+    const [valorInput, setValorInput] = useState("");
+    const [hoveredIndex, setHoveredIndex] = useState(null);
 
-const [todoList, setTodoList] = useState(["HACER LA CAMA", "HACER LA LOZA", "SACAR A LOS PERROS"])
-const [valorinput, setValorInput] = useState("")
-const [hoveredIndex, setHoveredIndex] = useState(null)
+    const getUserTasks = () => {
+        fetch("https://playground.4geeks.com/todo/users/sofia")
+        .then (resp => {
+            console.log(resp);
+            if (resp.ok === false) {
+                creatUser()
+            } else  return resp.json()})
+        .then(data => {setTodoList(data.todos)})
+        .catch(error => console.log(error))
+    };
 
-const handleChange = (e) => {
-	const newValue = e.target.value;
-	setValorInput(newValue);
-}
+    const creatUser = () => {
+        fetch("https://playground.4geeks.com/todo/users/sofia", {
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json"
+            },
+        })
+        .then(resp => resp.json())
+        .then(data => getUserTasks())
+        .catch(error => console.log(error))
 
-const handleSubmit = (e) => {
-	e.preventDefault();
-	const newTodolist =[...todoList, valorinput] 
-	setTodoList(newTodolist)
-	setValorInput("")
-}
+    }
+    useEffect(() => {
+        getUserTasks();
+    }, []
+    );
 
-const handleOnClick = (index) => {
-    const eliminarElemento = [...todoList]
-	eliminarElemento.splice(index, 1)
-	setTodoList(eliminarElemento)
-}
+    const createTask = (task) => {
+        fetch("https://playground.4geeks.com/todo/todos/sofia", {
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify({
+                label: task,
+                is_done: false
+              })
+        })
+        .then(resp => resp.json())
+        .then(data => getUserTasks())
+        .catch(error => console.log(error))
+    };
 
-const handleMouseEnter = (index) => {
-setHoveredIndex(index)
-}
+    const handleChange = (e) => {
+        const newValue = e.target.value;
+        setValorInput(newValue);
+    };
 
-const handleMouseLeave = () => {
-	setHoveredIndex(null)
-}
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        createTask(valorInput);
+        setValorInput("");
+    };
+    console.log(todoList);
+    const handleMouseEnter = (index) => {
+        setHoveredIndex(index);
+    }
 
-return (
+    const handleMouseLeave = () => {
+        setHoveredIndex(null);
+    };
+
+    const handleOnclick = (id) =>{
+        fetch("https://playground.4geeks.com/todo/todos/"+id, {
+            method: "DELETE",
+            headers: {
+                "Content-Type":"application/json"
+            },
+        })
+        .then(resp => resp.text())
+        .then(data => getUserTasks())
+        .catch(error => console.log(error))
+        };
+
+        const deleteAll = () => {
+            fetch("https://playground.4geeks.com/todo/users/sofia", {
+                method: "DELETE",
+                headers: {
+                    "Content-Type":"application/json"
+                },
+            })
+            .then(resp => resp.text())
+            .then(data => getUserTasks())
+            .catch(error => console.log(error))
+        }
+
+    return (
     <div className="bg-light d-flex flex-column justify-content-start align-items-center min-vh-100">
         <h1 className="display-3 text-danger text-opacity-25">todos</h1>
         <div className="card bg-light rounded-0 shadow w-50">
             <div className="card-body text-justify">
                 <form onSubmit={handleSubmit}>
-                    <input className="lead border-0 m-2 p-2 w-75" type="text" placeholder="What needs to be done?" value={valorinput} onChange={handleChange}/>
+                    <input className="lead border-0 m-2 p-2 w-75" type="text" placeholder="What needs to be done?" value={valorInput} onChange={handleChange}/>
                 </form>
                 <ul className="lead list-group list-group-flush list-unstyled">
-                    {todoList.map((todo, index) => (
+                    {todoList.map((elemento, index) => (
                         <li className="d-flex justify-content-between m-1 list-group-item bg-light" key={index} onMouseEnter={() => handleMouseEnter(index)}onMouseLeave={handleMouseLeave}>
-                            {todo}
-                            {hoveredIndex === index && (<span><button onClick={()=>handleOnClick(index)} >X</button></span>) }</li>
+                            {elemento.label}
+                            {hoveredIndex === index && (<span><button className="border-0 bg-light text-danger" onClick={()=>handleOnclick(elemento.id)}>x</button></span>)}
+                        </li>
                     ))}
                 </ul>
                 {todoList.length > 0 && (
@@ -55,10 +116,10 @@ return (
                 {todoList.length === 0 &&(
                     <p className="lead fs-6">No hay tareas, añadir tareas.</p>
                 )}
+                <button onClick={deleteAll} >DELETE ALL</button>
             </div>
         </div>
     </div>
     );
 };
 export default Home;
-
